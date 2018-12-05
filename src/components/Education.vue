31160754e0116a7bd32f72036c977ae8e4161809
b-45 <bernard11ade@gmail.com>
2018-12-04 20:03:39 -0500

    correct typo

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -2,7 +2,7 @@
   <section class="Education text-sm mb-4">
     <div class="font-semibold">Education</div>
     <div class="leading-normal">
-      <div class="company">Bachelof of Arts - Business Administration</div>
+      <div class="company">Bachelor of Arts - Business Administration</div>
       <div class="school">Lincoln University - Oxford, Pennsylvania</div>
     </div>
   </section>
